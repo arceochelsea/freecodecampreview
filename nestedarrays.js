38de3504172ce0f34deb935fa82100e249a46137
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-04-09 19:28:07 -0400

    Added for loops

--- nestedarrays.js
+++ nestedarrays.js
@@ -3,3 +3,8 @@
 let candies = ['Fruity', 'Chocolatey', 'Peanut Butter'];
 
 let candyTypes = [['Fruit Roll Ups', 'Life Savers', 'Skittles'], ['Hersheys', 'Cadbury', 'Milky Way'], ['Reeses', 'Butterfinger', 'Mr. Good Bar']];
+
+for (i = 0; i < candyTypes.length; i++) {
+    for (j = 0; j < candies[i].length; j++) {
+    }
+}
