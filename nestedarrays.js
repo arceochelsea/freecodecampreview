eaafcb0f7fda87958131d21e971f89c2b737bcb3
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-04-09 19:25:09 -0400

    Added two arrays

---
+++ nestedarrays.js
@@ -0,0 +1,5 @@
+
+
+let candies = ['Fruity', 'Chocolatey', 'Peanut Butter'];
+
+let candyTypes = [['Fruit Roll Ups', 'Life Savers', 'Skittles'], ['Hersheys', 'Cadbury', 'Milky Way'], ['Reeses', 'Butterfinger', 'Mr. Good Bar']];
