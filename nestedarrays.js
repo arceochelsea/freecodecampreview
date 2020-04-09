c136e10a1e0ea9a4f167ed1a10a592d45b01c1f4
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-04-09 19:40:05 -0400

    Added console log and comment

--- nestedarrays.js
+++ nestedarrays.js
@@ -1,10 +1,10 @@
-
-
 let candies = ['Fruity', 'Chocolatey', 'Peanut Butter'];
 
 let candyTypes = [['Fruit Roll Ups', 'Life Savers', 'Skittles'], ['Hersheys', 'Cadbury', 'Milky Way'], ['Reeses', 'Butterfinger', 'Mr. Good Bar']];
 
-for (i = 0; i < candyTypes.length; i++) {
-    for (j = 0; j < candies[i].length; j++) {
+for (i = 0; i < candies.length; i++) {
+    for (j = 0; j < candyTypes[i].length; j++) {
+        console.log(`${candies[i]}: ${candyTypes[i][j]}`);
     }
 }
+//
